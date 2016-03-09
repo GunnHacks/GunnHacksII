ca2d3c1b9401e21b3ab1db289ee9b7993285fd28
justinyangusa <justin@y4ng.com>
2016-03-09 01:14:59 -0800

    Refactor map js

---
+++ js/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: 37.40252, lng: -122.1356052},
+    zoom: 16,
+    scrollwheel: false,
+    panControl: false,
+    mapTypeControl: false,
+    streetViewControl: false,
+  });
+}
